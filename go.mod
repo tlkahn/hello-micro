8f17619c840f00f28e8d4ac32517f5c0ca06aa3c
toeinriver <joshgy@gmail.com>
2020-07-06 22:17:16 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/tlkhan/hello-micro
+
+go 1.14
+
+require (
+	github.com/golang/protobuf v1.4.2
+	google.golang.org/grpc v1.30.0
+)
---
+++ server/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	// Import the generated protobuf code
+	pb "github.com/tlkhan/hello-micro/server/proto/hello"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	port = ":50051"
+)
+
+type service struct {
+}
+
+// CreateConsignment - we created just one method on our service,
+// which is a create method, which takes a context and a request as an
+// argument, these are handled by the gRPC server.
+func (s *service) sayHello(ctx context.Context, name *pb.Name) (*pb.Response, error) {
+	fmt.Printf("hello %s", name.name)
+
+}
+
+type service struct {
+}
+
+// CreateConsignment - we created just one method on our service,
+// which is a create method, which takes a context and a request as an
+// argument, these are handled by the gRPC server.
+func (s *service) sayHello(ctx context.Context, name *pb.Name) (*pb.Response, error) {
+
+	return &pb.Response(&pb.Greeting{greeting_words: *name.name})
+
+}
+
+func main() {
+
+	// Set-up our gRPC server.
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+
+	// Register our service with the gRPC server, this will tie our
+	// implementation into the auto-generated interface code for our
+	// protobuf definition.
+	pb.RegisterHelloServiceServer(s, &service{})
+
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
